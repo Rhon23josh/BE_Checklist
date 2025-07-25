1cf91e99012aa74a8f36bb2a06c07bfc314f5441
Rhon23josh <216458902+Rhon23josh@users.noreply.github.com>
2025-07-25 10:53:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,44 @@ const urlsToCache = [
   "/BE_Checklist/styles.css",
   "/BE_Checklist/script.js",
   // Add PDFs if you want them offline:
-  // "/BE_Checklist/Policies/file1.pdf",
-  // "/BE_Checklist/Policies/file2.pdf",
+  // "/BE_Checklist/Policies/BE_1_Implementation of Existing Security Measures.pdf",
+  // "/BE_Checklist/Policies/BE_2_Cash Handling Guidelines_Reiteration.pdf",
+    // "/BE_Checklist/Policies/BE_3_Disbursement and Liquidation of the Fund.pdf",
+    // "/BE_Checklist/Policies/BE_4_Cash Within EO Common Areas.pdf",
+    // "/BE_Checklist/Policies/BE_5_Sales Invoice - Signaure of Branch Managers.pdf",
+    // "/BE_Checklist/Policies/BE_6_Memo on the Disposal of Blue Copies.pdf",
+    // "/BE_Checklist/Policies/BE_7_Guidelines for Accounts Receivables.pdf",
+    // "/BE_Checklist/Policies/BE_8_Accounts Receivables.pdf",
+    // "/BE_Checklist/Policies/BE_9_Conditions on Deposit Requirement.pdf",
+    // "/BE_Checklist/Policies/BE_11_ Guidelines in Forfeiture of Accounts Receivable.pdf",
+    // "/BE_Checklist/Policies/BE_13_RETURNING OF ENGINEERING ITEMS TO ED - UPDATED.pdf",
+    // "/BE_Checklist/Policies/BE_13_Fixed assets disposal.pdf",
+    // "/BE_Checklist/Policies/BE_14_EO-ED STOCK TRANSFER TO ENGINEERING - OPTICAL EQUIPMENT.pdf",
+    // "/BE_Checklist/Policies/BE_14_Fixed Asset Transaction.pdf",
+    // "/BE_Checklist/Policies/BE_15_COMPANY PHONE POLICY.pdf",
+    // "/BE_Checklist/Policies/BE_16_Guidelines for Handling Damaged Lenses.pdf",
+    // "/BE_Checklist/Policies/BE_16_Unposted and Un-uploaded DRs.pdf",
+    // "/BE_Checklist/Policies/BE_17_Policy on MB.pdf",
+    // "/BE_Checklist/Policies/Be_18_Reiteration of Damage Policy.pdf",
+    // "/BE_Checklist/Policies/BE_19_CL Expiration Policy.pdf",
+    // "/BE_Checklist/Policies/BE_20_Employee Product Purchase Benefits .pdf",
+    // "/BE_Checklist/Policies/BE_20_Memo on Reserved Items for EPPB (Internal Agreement).pdf",
+    // "/BE_Checklist/Policies/BE_21_Strict Implementation of Existing Security Measures.pdf",
+    // "/BE_Checklist/Policies/BE_22_2023 Policy on Stock Transfer of Merchandise  Merchandise Inventory.pdf",
+  // "/BE_Checklist/Policies/BE_24_Lifetime Guarantee Card.pdf",
+  // "/BE_Checklist/Policies/BE_26_COLLECTION DISPLAY GUIDELINES 2024.pdf",
+  // "/BE_Checklist/Policies/BE_26_HOUSE BRAND MEMO.pdf",
+  // "/BE_Checklist/Policies/BE_27_Operations Audit Mandate per Office of the President.pdf",
+  // "/BE_Checklist/Policies/BE_28_RE Permits  Licenses.pdf",
+  // "/BE_Checklist/Policies/BE_30_Standard Charging for Non Displaying of Monthly POPs.pdf",
+  // "/BE_Checklist/Policies/BE_30_Amendment of Marketing Collaterals Guidelines.pdf",
+  // "/BE_Checklist/Policies/BE_31_Prohibition on Displaying Duplicate Items.pdf",
+  // "/BE_Checklist/Policies/BE_35_Spiel on CEG Processing Time.pdf",
+  // "/BE_Checklist/Policies/BE_37_Encoding and submission of Corporate Acct.pdf",
+  // "/BE_Checklist/Policies/BE_41_Timekeeping policy.pdf",
+  // "/BE_Checklist/Policies/BE_54_Simplified Policy on Lens Only Transaction .pdf",
+  // "/BE_Checklist/Policies/BE_55_Policy on Unclaimed Fully Paid Items.pdf",
+  // "/BE_Checklist/Policies/Be_56_Reiteration of Damage Policy.pdf",
 ];
 
 function runSearch() {
@@ -71,6 +107,7 @@ self.addEventListener("fetch", (event) => {
     );
   }
 });
+
 
 // PWA SERVICE WORKER REGISTRATION
 if ("serviceWorker" in navigator) {
